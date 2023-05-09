6b9c4218cc468078b79a7210cfb2773d9d2c88ee
agustinabalsas <f100408@net.in.iantel.com.uy>
2023-05-09 12:53:21 -0300

    1st commit

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GraficoHorizontalComponent } from './grafico-horizontal/grafico-horizontal.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    GraficoHorizontalComponent
+  ],
+  exports: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class ComponentsModule { }
---
+++ src/app/components/grafico-horizontal/grafico-horizontal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafico-horizontal',
+  templateUrl: './grafico-horizontal.component.html',
+  styleUrls: ['./grafico-horizontal.component.css']
+})
+export class GraficoHorizontalComponent {
+
+}
